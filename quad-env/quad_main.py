caad6cbbac0661dd2c2ed486d21d3150eb6b3d4e
harleenbrar <harleenbrar9@gatech.edu>
2020-04-19 16:02:00 -0400

    added quad_main.py file to use with environment and ddp

---
+++ quad-env/quad_main.py
@@ -0,0 +1,19 @@
+""" main file for quadrotor tracking with ddp controller """
+
+import params
+from simplequad import SimpleQuadEnv
+import pdb
+
+if __name__ == "__main__":
+
+    # create quadrotor environment
+    quad = SimpleQuadEnv()
+
+    pdb.set_trace()
+    
+
+
+
+
+
+
