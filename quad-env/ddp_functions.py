ca6a7b3f51ee94873c21893c46760a12547d2670
harleenbrar <harleenbrar9@gatech.edu>
2020-04-20 00:21:59 -0400

    add linearized dynamics function for ddp and begin the backward pass to evaluate the value function

--- quad-env/ddp_functions.py
+++ quad-env/ddp_functions.py
@@ -46,13 +46,75 @@ def state_control_transition(sys, x, u):
     B = np.zeros([controllers, controllers])
 
 
+    phi = x[6]
+    theta = x[7]
+    psi = x[8]
+    phi_dot = x[9]
+    theta_dot = x[10]
+    psi_dot = x[11]
 
+    f1 = u[0]
+    f2 = u[1]
+    f3 = u[2]
+    f4 = u[3]
 
-    return dfx, dfu
+    u1 = f1 + f2 + f3 + f4  # total force
+    u2 = f4 - f2            # roll actuation
+    u3 = f1 - f3            # pitch actuation
+    u4 = 0.05 * (f2 + f4 -f1 - f3) # yaw moment
 
+    A[0, 3] = 1
+    A[1, 4] = 1
+    A[2, 5] = 1
+    A[6, 9] = 1
+    A[7, 10] = 1
+    A[8, 11] = 1
 
+    A[3, 6] = (u1 * (np.cos(phi) * np.sin(psi) - np.cos(psi) * np.sin(phi) * np.sin(theta))) / m
+    A[3, 7] = (u1 * np.cos(phi) * np.cos(psi) * np.cos(theta)) / m
+    A[3, 8] = (u1 * (np.cos(psi) * np.sin(phi) - np.cos(phi) * np.sin(psi) * np.sin(theta))) / m
+    A[4, 6] = -(u1 * (np.cos(phi) * np.cos(psi) + np.sin(phi) * np.sin(psi) * np.sin(theta))) / m
 
+    A[4, 7] = (u1 * np.cos(phi) * np.cos(theta) * np.sin(psi)) / m
+    A[4, 8] = (u1 * (np.sin(phi) * np.sin(psi) + np.cos(phi) * np.cos(psi) * np.sin(theta))) / m
+    A[5, 6] = (u1 * np.cos(theta) * np.sin(phi)) / m
 
+    A[9, 10] = (psi_dot * (Jy - Jz)) / Jx
+    A[9, 11] = (theta_dot * (Jy - Jz)) / Jx
+
+    A[10, 9] = -(psi_dot * (Jx - Jz)) / Jy
+    A[10, 11] = -(phi_dot * (Jx - Jz)) / Jy
+
+    A[11, 9] = (theta_dot * (Jx - Jy)) / Jz
+    A[11, 10] = (phi_dot * (Jx - Jy)) / Jz
+
+    B[3, 0] = (np.sin(phi) * np.sin(psi) + np.cos(phi) * np.cos(psi) * np.sin(theta)) / m
+    B[3, 1] = (np.sin(phi) * np.sin(psi) + np.cos(phi) * np.cos(psi) * np.sin(theta)) / m
+    B[3, 2] = (np.sin(phi) * np.sin(psi) + np.cos(phi) * np.cos(psi) * np.sin(theta)) / m
+    B[3, 3] = (np.sin(phi) * np.sin(psi) + np.cos(phi) * np.cos(psi) * np.sin(theta)) / m
+
+    B[4, 0] = -(np.cos(psi) * np.sin(phi) - np.cos(phi) * np.sin(psi) * np.sin(theta)) / m
+    B[4, 1] = -(np.cos(psi) * np.sin(phi) - np.cos(phi) * np.sin(psi) * np.sin(theta)) / m
+    B[4, 2] = -(np.cos(psi) * np.sin(phi) - np.cos(phi) * np.sin(psi) * np.sin(theta)) / m
+    B[4, 3] = -(np.cos(psi) * np.sin(phi) - np.cos(phi) * np.sin(psi) * np.sin(theta)) / m
+
+    B[5, 0] = -(np.cos(phi) * np.cos(theta)) / m
+    B[5, 1] = -(np.cos(phi) * np.cos(theta)) / m
+    B[5, 2] = -(np.cos(phi) * np.cos(theta)) / m
+    B[5, 3] = -(np.cos(phi) * np.cos(theta)) / m
+
+    B[9, 1] = -L/Jx
+    B[9, 3] = L/Jx
+
+    B[10, 0] = L/Jy
+    B[10, 2] = -L/Jy
+
+    B[11, 0] = -1 / (20 * Jz)
+    B[11, 1] = 1 / (20 * Jz)
+    B[11, 2] = -1 / (20 * Jz)
+    B[11, 3] = 1 / (20 * Jz)
+
+    return A, B
 
 
 def ddp(sys, x, u):
@@ -100,10 +162,20 @@ def ddp(sys, x, u):
         # linearize dynamics
         dfx, dfu = state_control_transition(sys, x[:, t], u[:, t])
 
+        A[:, :, t] = np.eye(states, states) + dfx * dt
+        B[:, :, t] = dfu * dt
+
+    # back prop for value function
+    V[V.shape[1]] = 0.5 * (x[:, x.shape[1]] - xf).T.dot(Qf).dot(x[:, x.shape[1]] - xf)
+    Vx[:, Vx.shape[1]] = Qf.dot(x[:, x.shape[1]] - xf)
+    Vxx[:, :, Vxx.shape[2]] = Qf
+
+    for t in range((timesteps-1), -1, -1):
+        # get state action value function to evalueate the linearized bellman equation
 
 
 
-        pdb.set_trace()
+    pdb.set_trace()
 
 
 
