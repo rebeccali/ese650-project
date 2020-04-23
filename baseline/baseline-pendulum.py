731fbaa3c08257187eeab8beb3a29898ce0d8988
harleenbrar <harleenbrar9@gatech.edu>
2020-04-23 14:02:16 -0400

    add dataset functions

--- baseline/baseline-pendulum.py
+++ baseline/baseline-pendulum.py
@@ -3,6 +3,8 @@ import autograd
 import autograd.numpy as np
 
 import scipy.integrate
+import pdb
+
 
 solve_ivp = scipy.integrate.solve_ivp
 
@@ -21,21 +23,22 @@ noise = 0.5
 
 
 def pend_dynamics(t, x0):
-    theta = x0[0]
-    thetadot = x0[1]
 
-    dxdt = np.zeros([states, 1])
-    dxdt[0] = thetadot
-    dxdt[1] = -g / L * np.sin(theta)
+    theta, thetadot = np.split(x0, 2)
+
+    dtheta = thetadot
+    dthetadot = -g / L * np.sin(theta)
+
+    dxdt = np.concatenate([dtheta, dthetadot], axis = -1)
 
     return dxdt
 
 
-def get_trajectory(radius=None, x0=None, **kwargs):
+def get_trajectory(t_span, timescale, noise_std, radius=None, x0=None, **kwargs):
     """ takes in the batch size and outputs the state trajectory from a randomly sampled intitial condition """
-    t_span = [0, 3]
-    timescale = 15  # size of trajectory ~~ is this same as batch size???
-    noise_std = 1
+    # t_span = [0, 3]
+    # timescale = 15  # this is the discretization per second, size of the trajectory is 45 in this case
+    # noise_std = 1
 
     t_eval = np.linspace(t_span[0], t_span[1], int(timescale * (t_span[1] - t_span[0])))
 
@@ -47,36 +50,46 @@ def get_trajectory(radius=None, x0=None, **kwargs):
         radius = np.random.rand() + 1.3
 
     # radius is scaled by the norm of the state vector
-    x0 = x0 / (np.sqrt(x0 ** 2).sum()) * radius
+    x0 = x0 / np.sqrt((x0 ** 2).sum()) * radius
 
     # solve ivp with scipy ivp solver
+    y = solve_ivp(fun=pend_dynamics, t_span=t_span, y0=x0, t_eval=t_eval, rtol=1e-10)
 
-    x = solve_ivp(fun=pend_dynamics, t_span=t_span, y0=x0, t_eval=t_eval, rtol=1e-10)
+    x = y['y']
+    x += np.random.randn(*x.shape) * noise_std
 
-    # add noise to states
-    x += np.random.randn(states) * noise_std
-
-    return x, x0
+    return x
 
 
 def get_dataset():
     """ gets the dataset """
 
-    samples = 50  # number of batches
+    num_samples = 50  # number of batches
     split = 0.5
     seed = 0
+
+    # params for each specific trajectory
+    t_span = [0, 3]
+    timescale = 15  # this is the discretization per second, size of the trajectory is 45 in this case
+    noise_std = 1
+
 
     # randomly sample inputs
     np.random.seed(seed)
 
-    x = []
-    dx = []
+    x = np.zeros([num_samples, states, timescale * (t_span[1] - t_span[0])])
 
-    for s in range(samples):
+    for s in range(num_samples):
         # get batch of samples
-        x, x0 = get_trajectory()
+        x_sample = get_trajectory(t_span, timescale, noise_std)
+        x[s, :, :] = x_sample
 
-    return data
+    test_size = int(np.ceil(num_samples/2))
+
+    test_data = x[:test_size, :, :]
+    train_data = x[test_size:, :, :]
+
+    return test_data, train_data
 
 
 if __name__ == "__main__":
