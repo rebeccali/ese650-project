ecbfba3f46ed9759482f1011a82947bc327fca26
Walker Gosrich <wgosrich@gmail.com>
2020-04-26 16:02:33 -0400

    ok really merged in hamiltonian this time

--- environments/simplequad.py
+++ environments/simplequad.py
@@ -105,6 +105,24 @@ class SimpleQuadEnv(gym.Env):
         # cost = 0.5 * np.matmul(delta_x.T, np.matmul(Q, delta_x)) + 0.5 * np.matmul(u.T, np.matmul(R, u))
 
         return -cost
+    
+    def get_H(self):
+        dx = self.state[3:6]
+        dth = self.state[9:12]
+        z = self.state[2]
+        
+        trans = (1/2)*self.m*np.dot(dx,dx)
+        rot = (1/2)*(dth.T @ self.J) @ dth
+        
+        return trans + rot - self.m*self.g*z
+    
+    def get_pdot(self):
+        pdot = np.zeros((6,))
+        pdot[2] = self.m*self.g
+        return pdot
+
+    def get_qdot(self):
+        return np.concatenate((self.state[3:6],self.state[9:12]))
 
     def set_goal(self, goal):
         # takes a size (12,) numpy array representing the goal state against which rewards should be measured
