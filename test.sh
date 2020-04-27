a9999952dfdca08739b009a44aabc6d46967e161
Rebecca Li <rmli@protonmail.ch>
2020-04-27 12:30:44 -0400

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python ddp_main.py --test
 python quad_main.py --test
-python symplectic-ODENet/experiment-single-force/train.py --verbose --baseline --total_steps 10
-#python experiment-single-force/train.py --verbose --total_steps 10
-#python experiment-single-force/train.py --verbose --structure --total_steps 10
-#python experiment-single-force/train.py --verbose --naive --total_steps 10
+python experiment_single_embed/train.py --verbose --baseline --total_steps 10
+#python experiment_single_embed/train.py --verbose --total_steps 10
+#python experiment_single_embed/train.py --verbose --structure --total_steps 10
+#python experiment_single_embed/train.py --verbose --naive --total_steps 10
