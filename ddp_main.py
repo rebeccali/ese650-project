9b39a962b7e222a056dccbcce8757e012e670a8d
Rebecca Li <rmli@protonmail.ch>
2020-04-28 10:31:37 -0400

    Added env as argument to ddp_main

--- ddp_main.py
+++ ddp_main.py
@@ -10,24 +10,21 @@ import numpy as np
 import pdb
 
 if __name__ == "__main__":
+    envs = ['DDP-Pendulum-v0', 'Simple-Quad-v0']
     # Parse Command Line Arguments
     # e.g. python ddp_main.py --test
     parser = argparse.ArgumentParser(description='Run DDP')
-    parser.add_argument('--test', action='store_true',
-                        help='Run as test')
+    parser.add_argument('--test', action='store_true', help='Run as test')
+    parser.add_argument('--env', help='Which environment to use.', default=envs[0], choices=envs)
     args = parser.parse_args()
 
-    ################################ system specific stuff ###################################
-
-    sys = gym.make('DDP-Pendulum-v0')
-    # sys = gym.make('Simple-Quad-v0')
-
-    ################################################################################################
+    sys = gym.make(args.env)
 
     num_iter = sys.num_iter
     if args.test:
         num_iter = 3
-
+        print('Running in test mode.')
+    print('Using %s environment for %d iterations' % (args.env, num_iter))
     x = np.zeros([sys.states, sys.timesteps])
     u = np.zeros([sys.num_controllers, sys.timesteps - 1])
 
