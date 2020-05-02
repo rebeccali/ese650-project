94602a30b9119fc8b26834a609a19217938aae6c
Walker Gosrich <wgosrich@gmail.com>
2020-05-02 13:38:18 -0400

    modified gitignore to ignore trained models, added new get_all_models_and_stats to analysis.py

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python ddp_main.py --test
 python quad_main.py --test
 python experiment_single_embed/train.py --verbose --baseline --total_steps 10
-#python experiment_single_embed/train.py --verbose --total_steps 10
-#python experiment_single_embed/train.py --verbose --structure --total_steps 10
-#python experiment_single_embed/train.py --verbose --naive --total_steps 10
+python experiment_single_embed/train.py --verbose --total_steps 10
+python experiment_single_embed/train.py --verbose --structure --total_steps 10
+python experiment_single_embed/train.py --verbose --naive --total_steps 10
