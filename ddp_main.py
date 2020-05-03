016911e6e8d9a2f0b849e685e8e6dd025b8a186d
Rebecca Li <rmli@protonmail.ch>
2020-05-03 10:39:52 -0400

    Refactored ddp_main to accept learned_pendulum

--- ddp_main.py
+++ ddp_main.py
@@ -1,6 +1,6 @@
 """ main file for quadrotor tracking with ddp controller """
 
-from ddp.ddp_functions import *
+from ddp.ddp_functions import apply_control, ddp
 import matplotlib.pyplot as plt
 import gym
 import environments
@@ -11,18 +11,42 @@ import pdb
 
 
 def main(args):
-    sys = gym.make(args.env)
-    num_iter = sys.num_iter
+
+    if args.env in environments.learned_models:
+        # TODO make this less manual
+        if args.env == 'LearnedPendulum-v0':
+            from environments.learned_pendulum import LearnedPendulumEnv
+            env = LearnedPendulumEnv(model_type='structure')
+        else:
+            raise RuntimeError('Do not recognized learned model %s ' % args.env)
+    else:
+        env = gym.make(args.env)
+
+    num_iter = env.num_iter
     if args.test:
         num_iter = 3
         print('Running in test mode.')
     print('Using %s environment for %d iterations' % (args.env, num_iter))
-    x = np.zeros([sys.states, sys.timesteps])
-    u = np.zeros([sys.num_controllers, sys.timesteps - 1])
+    costvec, u, x, xf = run_ddp(env, num_iter)
+    env.plot(xf, x, u, costvec)
+    if not args.test:
+        plt.show()
+
+
+def run_ddp(env, num_iter):
+    """Run DDP on environment for num_iter timesteps
+        Returns:
+            costvec(nparray): costs at each timestep
+            u(nparray): the control trajectory
+            x(nparray): the state trajectory
+            xf(nparray): the system goal
+    """
+    x = np.zeros([env.states, env.timesteps])
+    u = np.zeros([env.num_controllers, env.timesteps - 1])
     costvec = []
     for i in range(num_iter):
-        u_opt = ddp(sys, x, u)
-        x_new, cost = apply_control(sys, u_opt)
+        u_opt = ddp(env, x, u)
+        x_new, cost = apply_control(env, u_opt)
 
         # update state and control trajectories
         x = x_new
@@ -32,20 +56,18 @@ def main(args):
         costvec.append(-cost)
 
         # reset the system so that the next optimization step starts from the correct initial state
-        sys.reset()
+        env.reset()
 
         print('iteration: ', i, "cost: ", -cost)
-    xf = sys.goal
+    xf = env.goal
     x = np.asarray(x)
     u = np.asarray(u)
     costvec = np.asarray(costvec)
-    sys.plot(xf, x, u, costvec)
-    if not args.test:
-        plt.show()
+    return costvec, u, x, xf
 
 
 if __name__ == "__main__":
-    envs = ['DDP-Pendulum-v0', 'Simple-Quad-v0']
+    envs = ['DDP-Pendulum-v0', 'Simple-Quad-v0', 'LearnedPendulum-v0']
     # Parse Command Line Arguments
     # e.g. python ddp_main.py --test
     parser = argparse.ArgumentParser(description='Run DDP')
